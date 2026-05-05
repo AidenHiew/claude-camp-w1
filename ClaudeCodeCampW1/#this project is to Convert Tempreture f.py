994852d15070d50c10acad315d7e41548929bda5
AidenHiew <281902732+AidenHiew@users.noreply.github.com>
2026-05-05 23:07:53 +0930

    Add temperature converter project

---
+++ ClaudeCodeCampW1/#this project is to Convert Tempreture f.py
@@ -0,0 +1,12 @@
+#this project is to Convert Tempreture from Fahrenheit to Celsius
+#The fomula (32°F − 32) × 5/9 = 0°C
+#32'F is 0'C
+ 
+fahrenheit = 50
+celsis = (fahrenheit -32) *(5/9)
+print(celsis)
+
+print (f"{fahrenheit}'F is {celsis}'C")
+
+print (f"{celsis}'C is {fahrenheit}'F")
+
