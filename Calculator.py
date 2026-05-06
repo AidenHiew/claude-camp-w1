abdbdd3ae1aa1d7c9409ccc86e74888e52798359
AH-TRCA <280573176+AH-TRCA@users.noreply.github.com>
2026-05-06 13:23:20 +0930

    done, a simple calculator

---
+++ Calculator.py
@@ -0,0 +1,15 @@
+#Calculator
+
+#This is a simple calculator program that can perform basic arithmetic operations such as addition, subtraction, multiplication, and division. The user can input two numbers and select the desired operation to get the result.
+
+print ("Welcome to the simple calculator!")
+
+print ("Please select the number here")
+X = int(input ("What is X?"))
+Y = int(input ("What is Y?"))
+Z = (X) * (Y)
+
+print ("Confirming your numbers: X=", X, "and Y=", Y)
+print ("The outcome is", Z)
+
+
